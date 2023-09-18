ad3d3346a251beba9c60e4dfab6accdd046e8f06
looju <omofade2020@gmail.com>
2023-09-18 18:09:59 +0100

    cp

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -20,7 +20,7 @@ module.exports = {
         xs: "450px",
       },
       backgroundImage: {
-        "hero-pattern": "url('/src/assets/herobg7.jpg')",
+        "hero-pattern": "url('/src/assets/herobg10.jpg')",
       },
     },
   },
