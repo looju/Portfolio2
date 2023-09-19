3bdb750c128601662f69f531d21d08ba5e444cfc
looju <omofade2020@gmail.com>
2023-09-19 20:59:58 +0100

    fix earth and star

--- src/components/canvas/Earth.jsx
+++ src/components/canvas/Earth.jsx
@@ -1,14 +1,15 @@
 import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
-import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
-
+import { OrbitControls, Preload, useGLTF,Points, PointMaterial, } from "@react-three/drei";
+import { Stars } from "./Stars";
+import * as random from "maath/random/dist/maath-random.esm";
 import CanvasLoader from "../Loader";
 
 const Earth = () => {
   const earth = useGLTF("./planet/scene.gltf");
 
   return (
-    <primitive object={earth.scene} scale={2.5} position-y={0} rotation-y={0} />
+    <primitive object={earth.scene} scale={0.5} position-y={0} rotation-y={0} />
   );
 };
 
@@ -16,14 +17,14 @@ const EarthCanvas = () => {
   return (
     <Canvas
       shadows
-      frameloop='demand'
+      frameloop="demand"
       dpr={[1, 2]}
       gl={{ preserveDrawingBuffer: true }}
       camera={{
-        fov: 45,
-        near: 0.1,
-        far: 200,
-        position: [-4, 3, 6],
+        // fov: 45,
+        // near: 0.1,
+        // far: 200,
+        position: [0,0,1],
       }}
     >
       <Suspense fallback={<CanvasLoader />}>
@@ -33,8 +34,8 @@ const EarthCanvas = () => {
           maxPolarAngle={Math.PI / 2}
           minPolarAngle={Math.PI / 2}
         />
-        <Earth />
-
+       <Earth/>
+        <Stars />
         <Preload all />
       </Suspense>
     </Canvas>
